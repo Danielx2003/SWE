db3934aff8a82ff7ce14e7de98dad808999f52da
MichalKantarowski <michal.kantarowski@hatless.studio>
2024-02-28 18:45:35 +0000

    created endpoint for challenges.

--- backend/qrCodes/urls.py
+++ backend/qrCodes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import QRCodeListCreateView, QRCodeImageView, QRCodeScannedView
+from .views import QRCodeListCreateView, QRCodeImageView, QRCodeScannedView, ChallengeListView
 
 urlpatterns = [
     path('', QRCodeListCreateView.as_view(), name='qrcode-list-create'),
     path('<int:pk>/image/', QRCodeImageView.as_view(), name='qrcode-image'),
     path('find/<str:code>/', QRCodeScannedView.as_view(), name='qrcode-lookup'),
+    path('challenges/', ChallengeListView.as_view(), name='challenge-list'),
 ]
