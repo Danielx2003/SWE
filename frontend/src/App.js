13dd81b619d282ce31d9797f489fc52cdddbf287
cxlys <daniel.casley.2002@gmail.com>
2024-02-28 14:04:38 +0000

    Fixing the consequences of my merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,6 +48,7 @@ function App() {
           <Route
               path="/admin/create"
               element={< QRCreate />}
+              />
           <Route 
             path="/qr"
             element={<Congrats
