7fbffc962bf6ca797b5dc0b4c3fd028ae90e8ff7
MichalKantarowski <michal.kantarowski@hatless.studio>
2024-02-28 15:24:02 +0000

    created endpoint for leaderboard.

--- backend/garden/views.py
+++ backend/garden/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.pagination import PageNumberPagination
+
 from .models import GardenData
-from .serializers import GardenDataSerializer
+from .serializers import GardenDataSerializer, GardenLeaderboardSerializer
+
 
 class GardenDataDetail(generics.RetrieveAPIView):
     """
@@ -23,3 +26,29 @@ class GardenDataDetail(generics.RetrieveAPIView):
     def get_object(self):
         user_id = self.request.user.id
         return self.queryset.get(user__id=user_id)
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class GardenLeaderboardView(generics.ListAPIView):
+    """
+    API endpoint that allows garden leaderboard to be viewed.
+    method: GET
+    url: garden/leaderboard/?page=pagenumber&page_size=pagesize
+    ordered by points in descending order
+    successful response: [
+        {
+            "username": "username",
+            "points": "points"
+        },
+        ...
+    ]
+    """
+    serializer_class = GardenLeaderboardSerializer
+    pagination_class = CustomPagination
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = GardenData.objects.all().order_by('-points')
