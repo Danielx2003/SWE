0c6eee008b69b8856d1c80b01abb219acf2b51bc
MichalKantarowski <michal.kantarowski@hatless.studio>
2024-03-18 18:27:34 +0000

    added endpoint for searching through all users by admins.

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("register/", views.RegistrationView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
-    path("user/", views.UserDetailsView.as_view(), name="user-detail")
+    path("user/", views.UserDetailsView.as_view(), name="user-detail"),
+    path("user-search/", views.UserSearchView.as_view(), name="user-search"),
 ]
