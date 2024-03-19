941ed384afe9fbc730b65192d8808da35d06ddb3
Danielx2003 <ahipsterwalrus4@gmail.com>
2024-03-19 15:34:46 +0000

    added boldness to indicate what tag user is on

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,8 +16,8 @@ export default function Login(props) {
         <div className="login-container">
             <div className="form-container">
                 <div id="auth-buttons" className="auth-button-controller d-flex w-100 flex-row">
-                    <a id="left-auth" onClick={() => toggleAuthStateL(false)} className="auth-button d-flex w-50 justify-content-center border-end"><h4 class="form-title mb-0">LOGIN</h4></a>
-                    <a id="right-auth" onClick={() => toggleAuthStateR(true)} className="auth-button d-flex w-50 justify-content-center"><h4 class="form-title mb-0">REGISTER</h4></a>
+                    <a id="left-auth" onClick={() => toggleAuthStateL(false)} className="auth-button d-flex w-50 justify-content-center border-end"><h4 class="form-title mb-0" style={{fontWeight : !authState ? 'bold' : ""}}>LOGIN</h4></a>
+                    <a id="right-auth" onClick={() => toggleAuthStateR(true)} className="auth-button d-flex w-50 justify-content-center"><h4 class="form-title mb-0" style={{fontWeight : authState ? 'bold' : ""}}>REGISTER</h4></a>
                     <label type="hidden"></label>
                 </div>
                 <hr className="mt-0 mb-4"></hr>
