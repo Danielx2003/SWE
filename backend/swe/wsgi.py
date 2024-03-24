82627d01422a36b6f3219d6dc14a3a25e494cc4a
Danielx2003 <ahipsterwalrus4@gmail.com>
2024-03-24 23:54:04 +0000

    correct settings file on deploy

--- backend/swe/wsgi.py
+++ backend/swe/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-settings_modle = 'swe.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'swe.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swe.settings")
+settings_module = 'swe.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'swe.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",settings_module)
 
 application = get_wsgi_application()
