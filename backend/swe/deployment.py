a4c2552f9069a3d382fee28ed890d4a71c7c1443
Danielx2003 <ahipsterwalrus4@gmail.com>
2024-03-25 15:04:13 +0000

    added back csrf

--- backend/swe/deployment.py
+++ backend/swe/deployment.py
@@ -3,11 +3,9 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-# CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 SECRET_KEY = os.environ['MY_SECRET_KEY']
-
-    # 'django.middleware.csrf.CsrfViewMiddleware',
 
 
 MIDDLEWARE = [
@@ -16,6 +14,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -50,4 +49,3 @@ DATABASES = {
 
 
 STATIC_ROOT = BASE_DIR/'staticfiles'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
